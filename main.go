06d711a02b6714c570db1ef99dd55cbed5addbd2
nao11aihara <nao11aihara@gmail.com>
2022-03-15 00:40:10 +0900

    add: main.go

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Product struct {
+	Id	string	`json:"id"`
+	Title	string	`json:"title"`
+	Price	int	`json:"price"`
+}
+
+func main() {
+	e := echo.New()
+
+	api := e.Group("/api")
+
+	api.GET("/products", getProducts)
+	api.GET("/products/:id", getProduct)
+	api.POST("/products", postProduct)
+	api.PUT("/products/:id", putProduct)
+	api.DELETE("/products/:id", deleteProduct)
+
+	e.Start(":8080")
+}
+
+func getProducts(c echo.Context) error {
+	name := c.QueryParam("name")
+
+	fmt.Println("name:", name)
+	fmt.Println("商品一覧取得処理")
+
+	product1 := Product{
+		Id: "1",
+		Title: "商品1",
+		Price: 1000,
+	}
+	product2 := Product{
+		Id: "2",
+		Title: "商品2",
+		Price: 1000,
+	}
+
+	products := []Product{product1, product2}
+
+	return c.JSON(http.StatusOK, products)
+}
+
+func getProduct(c echo.Context) error {
+	id := c.Param("id")
+
+	fmt.Println("id:", id)
+	fmt.Println("商品取得処理")
+
+	product := Product{
+		Id: "1",
+		Title: "商品1",
+		Price: 1000,
+	}
+
+	return c.JSON(http.StatusOK, product)
+}
+
+func postProduct(c echo.Context) error {
+	fmt.Println("商品作成処理")
+
+	product := Product{
+		Id: "1",
+		Title: "商品1",
+		Price: 1000,
+	}
+
+	return c.JSON(http.StatusCreated, product)
+}
+
+func putProduct(c echo.Context) error {
+	id := c.Param("id")
+
+	fmt.Println("id:", id)
+	fmt.Println("商品更新処理")
+
+	product := Product{
+		Id: "1",
+		Title: "商品1",
+		Price: 1000,
+	}
+
+	return c.JSON(http.StatusOK, product)
+}
+
+func deleteProduct(c echo.Context) error {
+	id := c.Param("id")
+
+	fmt.Println("id:", id)
+	fmt.Println("商品削除処理")
+	
+	return c.String(http.StatusNoContent, "")
+}
