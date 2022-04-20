7677c8d882ccaaeac9d96459d015866d02de3f93
nao11aihara <nao11aihara@gmail.com>
2022-04-20 23:38:07 +0900

    add: configServer

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,1 +1,17 @@
 package controllers
+
+import "github.com/labstack/echo/v4"
+
+func ConfigServer() {
+	e := echo.New()
+
+	api := e.Group("/api")
+
+	api.GET("/products", getProducts)
+	api.GET("/products/:id", getProduct)
+	api.POST("/products", postProduct)
+	api.PUT("/products/:id", putProduct)
+	api.DELETE("/products/:id", deleteProduct)
+
+	e.Start(":8080")
+}
